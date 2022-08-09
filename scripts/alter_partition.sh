8c988c5cc4197707520faee34b34eccdac389e49
Kridtin Chawalratikool <kridtin.ch@gmail.com>
2022-08-09 20:08:58 +0000

    add container name

--- scripts/alter_partition.sh
+++ scripts/alter_partition.sh
@@ -1,4 +1,4 @@
 topic=$1
 total_partition=$2
 
-docker exec -d kafka-proj-demo-kafka-1 kafka-topics.sh --bootstrap-server localhost:9092 --alter --topic $topic --partitions $total_partition
+docker exec -d kafka kafka-topics.sh --bootstrap-server localhost:9092 --alter --topic $topic --partitions $total_partition
